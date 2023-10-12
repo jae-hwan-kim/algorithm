7834403ec62eb6083567a31b5da429f7b7f5407a
jae-hwan-kim <woghks7859@gmail.com>
2023-10-12 17:15:07 +0900

    WIP: 공원산책 문제 해결 중...

---
+++ 프로그래머스/2023.10.11/공원산책.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+
+using namespace std;
+
+vector<int> solution(vector<string> park, vector<string> routes) {
+    vector<int> answer;
+    int w = park[0].size() - 1;
+    int h = park.size() - 1;
+    
+    for (int i = 0; i < park.size(); i++) {
+        for (int j = 0; j < park[i].size(); j++) {
+            if (park[i][j] == 'S') {
+                answer.push_back(i);
+                answer.push_back(j);
+            }
+        }
+    }
+    for (int i = 0; i < routes.size(); i++) {
+        if (routes[i][0] == 'E') {
+            int move = 0;
+            int dest = atoi(&routes[i][2]);
+            if (answer[1] + dest > w) {
+                ;
+            } else {
+                for (int j = 1; j <= dest; j++) {
+                    if (park[answer[0]][answer[1] + j] == 'X') {
+                        move = 0;
+                        break;
+                    }
+                    move += 1;
+                }
+            }
+            answer[1] += move;
+        } else if (routes[i][0] == 'W') {
+            int move = 0;
+            int dest = atoi(&routes[i][2]);
+            if (answer[1] - dest > w) {
+                ;
+            } else {
+                for (int j = 1; j <= dest; j++) {
+                    if (park[answer[0]][answer[1] - j] == 'X') {
+                        move = 0;
+                        break;
+                    }
+                    move += 1;
+                }
+            }
+            answer[1] -= move;
+        } else if (routes[i][0] == 'S') {
+            int move = 0;
+            int dest = atoi(&routes[i][2]);
+            if (answer[0] + dest > w) {
+                ;
+            } else {
+                for (int j = 1; j <= dest; j++) {
+                    if (park[answer[0] + j][answer[1]] == 'X') {
+                        move = 0;
+                        break;
+                    }
+                    move += 1;
+                }
+            }
+            answer[0] += move;
+        } else if (routes[i][0] == 'N') {
+            int move = 0;
+            int dest = atoi(&routes[i][2]);
+            if (answer[0] + dest > w) {
+                ;
+            } else {
+                for (int j = 1; j <= dest; j++) {
+                    if (park[answer[0] - j][answer[1]] == 'X') {
+                        move = 0;
+                        break;
+                    }
+                    move += 1;
+                }
+            }
+            answer[0] -= move;
+        } 
+    }
+    return answer;
+}
+
+int main(void) {
+    // vector<string> park = {"SO0", "OOO", "OOO"};
+    // vector<string> routes = {"E 2", "S 2","W 1"};
+    // vector<string> park = {"SOO", "OXX", "OOO"};
+    // vector<string> routes = {"E 2","S 2","W 1"};
+    // vector<string> park = {"OSO","OOO","OXO","OOO"};
+    // vector<string> routes = {"E 2","S 3","W 1"}; // 1, 1
+    vector<string> park = {"OXXO", "XSXO", "XXXX"};
+    vector<string> routes = {"E 1", "S 1"};
+    solution(park, routes);
+    return (0);
+    // [2, 1]
+    // ["SOO","OXX","OOO"]	["E 2","S 2","W 1"]	[0,1]
+    // ["OSO","OOO","OXO","OOO"]	["E 2","S 3","W 1"]	[0,0]
+}
