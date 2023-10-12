2dd0e0c64d4b129b4213772c7654ee5d2cc204a4
jae-hwan-kim <woghks7859@gmail.com>
2023-10-12 17:14:51 +0900

    CHORE: 주석 삭제

--- 프로그래머스/2023.10.10/추억점수.cpp
+++ 프로그래머스/2023.10.10/추억점수.cpp
@@ -22,25 +22,6 @@ vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<st
     return answer;
 }
 
-// #include <string>
-// #include <vector>
-// #include <map>
-// using namespace std;
-
-// vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo)
-// {
-//     vector<int> answer;
-//     map<string, int> m;
-//     for(int i=0;i<name.size();i++) m[name[i]] = yearning[i];// key 이름, value 추억 점수
-//     for(int i=0;i<photo.size();i++)
-//     {
-//         int sum = 0;
-//         for(int j=0;j<photo[i].size();j++) sum += m[photo[i][j]];// 점수가 있으면 총점에 추가
-//         answer.push_back(sum);
-//     }
-//     return answer;
-// }
-
 int main() {
     vector<string> name = {"may", "kein", "kain", "radi"};
     vector<int> yearning = {5, 10, 1, 3};
